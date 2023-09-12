44720a6a7416e36321e860e03dc38199929bc3e7
Petit-Homme Wadly <wadleyp96@gmail.com>
2023-09-12 15:56:54 -0400

    tableau de bord complet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
+import Header from './components/header/Header';
+import Sidebar from './components/slidebar/Sidebar';
+import Home from './components/home/Home';
+import Footer from './components/footer/Footer';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const OpenSideBar = () => {
+    setIsOpen(!isOpen);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="grid-container">
+      <Header OpenSideBar={OpenSideBar} />
+      <Sidebar isOpen={isOpen} OpenSideBar={OpenSideBar} />
+      <Footer />
+      <Home />
+    </div >
   );
 }
 
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './header.css';
+import {
+    BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify
+} from 'react-icons/bs'
+
+
+const Header = ({ OpenSideBar }) => {
+
+    return (
+        <header className="header">
+            < div className="menu-icon" >
+                <BsJustify className="icon" onClick={OpenSideBar} />
+            </div >
+            <div className="header-left">
+                <BsSearch className="icon" />
+            </div>
+            <div className="header-right">
+                <BsFillBellFill className="icon" />
+                <BsFillEnvelopeFill className="icon" />
+                <BsPersonCircle className="icon" />
+            </div>
+        </header >
+    )
+}
+
+export default Header;
